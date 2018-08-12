310e0b208f2aee5df0db25db47442b5ea8b5c008
andinjo <anders.grinde@hotmail.com>
2018-08-12 22:14:14 +0200

    Fix bugs in sound and menu button

---
+++ Assets/Scripts/DestroyMusic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMusic : MonoBehaviour {
+	public void DestroyTheMusic () {
+        Destroy(GameObject.Find("Music"));
+	}
+}
