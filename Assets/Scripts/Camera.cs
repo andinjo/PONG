853ca025650ea33f407f2000b25e3e1aca2c339f
andinjo <anders.grinde@hotmail.com>
2018-08-12 22:34:14 +0200

    Add finished product, and learned how to do proper commits for next git project

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Camera : MonoBehaviour {
+
+    public void ChangeScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void DestroyTheMusic() {
+        Destroy(GameObject.Find("Music"));
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/DestroyMusic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DestroyMusic : MonoBehaviour {
-	public void DestroyTheMusic () {
-        Destroy(GameObject.Find("Music"));
-	}
-}
